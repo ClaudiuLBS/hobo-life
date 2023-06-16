38c2b5c5c974492c27ca8615a14547d3f15176e1
aciumalina <aciumalina@yahoo.com>
2023-06-16 13:38:08 +0300

    commented inventory item usage

--- LifeSimulator/Assets/Scripts/InventoryItem.cs
+++ LifeSimulator/Assets/Scripts/InventoryItem.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class InventoryItem : MonoBehaviour
 {
     public int index;
+    //metoda de folosire a unui item
     public void Use()
     {
+        //get item la index specificat
         Item item = PlayerMechanics.instance.inventory[index];
         item.Use();
+        //eliminarea obiectului
         PlayerMechanics.instance.inventory.RemoveAt(index);
+        //update inventar
         PlayerMenu.instance.UpdateInventory();
     }
+
+    //metoda de eliminare obiect
     public void Remove()
     {
         PlayerMechanics.instance.inventory.RemoveAt(index);
